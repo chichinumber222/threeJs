10b37816189beb5c8f7e118b620b5c3707f414ff
Ivan <chichi@bk.ru>
2024-10-07 14:38:09 +0300

    sdfsdf

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,190 @@
+// import * as THREE from 'three'
+// import GUI from 'lil-gui'
+// import { initScene, Props as InitSceneProps } from './bootstrap/bootstrap'
+// import { stats } from './utils/stats'
+// import { initHelpersControls } from './controls/helper-controls'
+// import { foreverFloor } from './bootstrap/floor'
+// import { initSceneControls } from './controls/scene-controls'
+
+// const props: InitSceneProps = {
+//   backgroundColor: new THREE.Color(0xffffff),
+//   disableDefaultControls: true,
+// }
+
+// const gui = new GUI()
+
+// const mountCube = (scene: THREE.Scene) => {
+//   const geometry = new THREE.BoxGeometry(0.5,0.5,0.5)
+//   const material = new THREE.MeshPhysicalMaterial({ color: '#3a7e57', clearcoat: 1, clearcoatRoughness: 0.2 })
+//   const cube = new THREE.Mesh(geometry, material)
+//   cube.castShadow = true
+//   cube.name = 'cube'
+//   scene.add(cube)
+//   return cube
+// }
+
+// const initCursorMovement = () => {
+//     const cursor = {
+//         x: 0,
+//         y: 0,
+//     }
+//     window.addEventListener('mousemove', (event) => {
+//         cursor.x = event.clientX / window.innerWidth - 0.5
+//         cursor.y = -(event.clientY / window.innerHeight - 0.5)
+//     })
+//     return cursor
+// }
+
+// initScene(props)(({ scene, camera, renderer }) => {
+//   camera.position.z = 3
+
+//   foreverFloor(scene, 10)
+
+//   const cube = mountCube(scene)
+
+//   const cursor = initCursorMovement()
+
+//   function animate() {
+//     camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+//     camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+//     camera.position.y = cursor.y * 10
+//     camera.lookAt(cube.position)
+
+//     requestAnimationFrame(animate)
+//     renderer.render(scene, camera)
+//     stats.update()
+//   }
+//   animate()
+
+//   initSceneControls(gui, scene)
+//   initHelpersControls(gui, scene)
+// })
+
 import * as THREE from 'three'
 import GUI from 'lil-gui'
-import { DragControls } from 'three/examples/jsm/controls/DragControls'
 import { initScene, Props as InitSceneProps } from './bootstrap/bootstrap'
 import { stats } from './utils/stats'
 import { initHelpersControls } from './controls/helper-controls'
-import { foreverFloor } from './bootstrap/floor'
+import { foreverFloor, foreverPlane } from './bootstrap/floor'
 import { initSceneControls } from './controls/scene-controls'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import gsap from 'gsap'
-
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 const props: InitSceneProps = {
-  backgroundColor: new THREE.Color(0xffffff),
+    backgroundColor: new THREE.Color(0xffffff),
+    disableDefaultControls: true,
 }
 
 const gui = new GUI()
 
 const mountCube = (scene: THREE.Scene) => {
-  const geometry = new THREE.BoxGeometry(3, 3, 3)
-  const material = new THREE.MeshPhysicalMaterial({ color: '#3a7e57', clearcoat: 1, clearcoatRoughness: 0.2 })
-  const cube = new THREE.Mesh(geometry, material)
-  cube.castShadow = true
-  cube.name = 'cube'
-  cube.position.y = -0.5
-  scene.add(cube)
-  return cube
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+    const material = new THREE.MeshPhysicalMaterial({ color: '#3a7e57', clearcoat: 1, clearcoatRoughness: 0.2 })
+    const cube = new THREE.Mesh(geometry, material)
+    cube.castShadow = true
+    cube.name = 'cube'
+    scene.add(cube)
+    return cube
 }
 
-const mountSpotLight = (scene: THREE.Scene) => {
-  const light = new THREE.SpotLight(0xffffff, 2)
-  light.position.set(4, 6, 2)
-  light.decay = 0.5
-  light.angle = 0.3
-  light.penumbra = 0.5
-  light.shadow.mapSize.width = 2048
-  light.shadow.mapSize.height = 2048
-  light.name = 'spot-light'
-  scene.add(light)
-  return light
-}
+initScene(props)(({ scene, camera, renderer }) => {
+    camera.position.z = 3
 
-const initDragControls = (scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.Renderer, orbitControls?: OrbitControls) => {
-  const controls = new DragControls(scene.children, camera, renderer.domElement)
-  controls.addEventListener('dragstart', ({ object }) => {
-    if (orbitControls) {
-      orbitControls.enabled = false
+    const controls = new PointerLockControls(camera, document.body)
+
+    controls.addEventListener('lock', function () {
+        controls.isLocked = false
+        console.log('lock')
+
+    })
+
+    controls.addEventListener('unlock', function () {
+        controls.isLocked = true
+        console.log('unlock')
+    })
+
+    let isLock = false
+    window.addEventListener('click', () => {
+        console.log('click')
+        isLock = !isLock
+        if (isLock) {
+            controls.lock()
+        } else {
+            controls.unlock()
+        }
+    })
+
+    const movement = {
+        w: false,
+        a: false,
+        s: false,
+        d: false
     }
-    if (object instanceof THREE.Mesh) {
-      object.material.emissive.set(0xffffff)
-      object.material.emissiveIntensity = 0.2
+
+    window.addEventListener('keydown', function (event) {
+        if (!controls.isLocked) {
+            return
+        }
+        switch (event.key) {
+            case 'w':
+            case 'W':
+                movement.w = true
+                break
+            case 'a':
+            case 'A':
+                movement.a = true
+                break
+            case 's':
+            case 'S':
+                movement.s = true
+                break
+            case 'd':
+            case 'D':
+                movement.d = true
+                break
+            default:
+                break
+        }
+    })
+
+    window.addEventListener('keyup', function (event) {
+        switch (event.key) {
+            case 'w':
+            case 'W':
+                movement.w = false
+                break
+            case 'a':
+            case 'A':
+                movement.a = false
+                break
+            case 's':
+            case 'S':
+                movement.s = false
+                break
+            case 'd':
+            case 'D':
+                movement.d = false
+                break
+            default:
+                break
+        }
+    })
+
+
+    foreverPlane(scene)
+
+    mountCube(scene)
+
+    function animate() {
+        if (movement.w) controls.moveForward(0.03)
+        if (movement.a) controls.moveRight(-0.03)
+        if (movement.s) controls.moveForward(-0.03)
+        if (movement.d) controls.moveRight(0.03)
+        requestAnimationFrame(animate)
+        renderer.render(scene, camera)
+        stats.update()
     }
-  })
-  controls.addEventListener('dragend', ({ object }) => {
-    if (orbitControls) {
-      orbitControls.enabled = true
-    }
-    if (object instanceof THREE.Mesh) {
-      object.material.emissive.set(0x000000)
-    } 
-  })
-  return controls
-}
+    animate()
 
-initScene(props)(({ scene, camera, renderer, orbitControls }) => {
-  camera.position.set(-7, 2, 5)
-  orbitControls?.update()
-
-  foreverFloor(scene, 10)
-
-  const cube = mountCube(scene)
-
-  //* Green sock animation platform (GSAP) делает анимацию прикольную
-  // gsap.to(cube.position, { delay: 2, duration: 3, y: 2})
-
-  mountSpotLight(scene)
-
-  initDragControls(scene, camera, renderer, orbitControls)
-
-  //* Чтобы на различным компьютерах с разным fps одинаковая скорость соблюдалась 1 вариант
-  // let time = Date.now()
-  // function animate() {
-
-  //   const currentTime = Date.now()
-  //   const deltaTime = currentTime - time
-  //   time = currentTime
-    
-  //   cube.rotation.y += 0.001 * deltaTime
-
-  //   requestAnimationFrame(animate)
-  //   renderer.render(scene, camera)
-  //   stats.update()
-
-  //   orbitControls?.update()
-  // }
-  // animate()
-  //*
-
-  //* Чтобы на различным компьютерах с разным fps одинаковая скорость соблюдалась 2 вариант
-  const clock  = new THREE.Clock()
-  function animate() {
-  
-    cube.rotation.y = clock.getElapsedTime() * Math.PI * 2
-
-    requestAnimationFrame(animate)
-    renderer.render(scene, camera)
-    stats.update()
-
-    orbitControls?.update()
-  }
-  animate()
-  //*
-
-  initSceneControls(gui, scene)
-  initHelpersControls(gui, scene)
+    initSceneControls(gui, scene)
+    initHelpersControls(gui, scene)
 })
+
