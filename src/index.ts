ef62485c71dff461d96d811424ab5d9db77587d0
Ivan <chichi@bk.ru>
2024-10-08 21:10:48 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,3 @@
-// import * as THREE from 'three'
-// import GUI from 'lil-gui'
-// import { initScene, Props as InitSceneProps } from './bootstrap/bootstrap'
-// import { stats } from './utils/stats'
-// import { initHelpersControls } from './controls/helper-controls'
-// import { foreverFloor } from './bootstrap/floor'
-// import { initSceneControls } from './controls/scene-controls'
-
-// const props: InitSceneProps = {
-//   backgroundColor: new THREE.Color(0xffffff),
-//   disableDefaultControls: true,
-// }
-
-// const gui = new GUI()
-
-// const mountCube = (scene: THREE.Scene) => {
-//   const geometry = new THREE.BoxGeometry(0.5,0.5,0.5)
-//   const material = new THREE.MeshPhysicalMaterial({ color: '#3a7e57', clearcoat: 1, clearcoatRoughness: 0.2 })
-//   const cube = new THREE.Mesh(geometry, material)
-//   cube.castShadow = true
-//   cube.name = 'cube'
-//   scene.add(cube)
-//   return cube
-// }
-
-// const initCursorMovement = () => {
-//     const cursor = {
-//         x: 0,
-//         y: 0,
-//     }
-//     window.addEventListener('mousemove', (event) => {
-//         cursor.x = event.clientX / window.innerWidth - 0.5
-//         cursor.y = -(event.clientY / window.innerHeight - 0.5)
-//     })
-//     return cursor
-// }
-
-// initScene(props)(({ scene, camera, renderer }) => {
-//   camera.position.z = 3
-
-//   foreverFloor(scene, 10)
-
-//   const cube = mountCube(scene)
-
-//   const cursor = initCursorMovement()
-
-//   function animate() {
-//     camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-//     camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-//     camera.position.y = cursor.y * 10
-//     camera.lookAt(cube.position)
-
-//     requestAnimationFrame(animate)
-//     renderer.render(scene, camera)
-//     stats.update()
-//   }
-//   animate()
-
-//   initSceneControls(gui, scene)
-//   initHelpersControls(gui, scene)
-// })
-
 import * as THREE from 'three'
 import GUI from 'lil-gui'
 import { initScene, Props as InitSceneProps } from './bootstrap/bootstrap'
@@ -67,36 +5,60 @@ import { stats } from './utils/stats'
 import { initHelpersControls } from './controls/helper-controls'
 import { foreverPlane } from './bootstrap/floor'
 import { initSceneControls } from './controls/scene-controls'
-import { initPointerControls } from './controller/pointer'
 const props: InitSceneProps = {
     backgroundColor: new THREE.Color(0xffffff),
-    disableDefaultControls: true,
 }
 
 const gui = new GUI()
 
 const mountCube = (scene: THREE.Scene) => {
-    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-    const material = new THREE.MeshPhysicalMaterial({ color: '#3a7e57', clearcoat: 1, clearcoatRoughness: 0.2 })
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5, 3, 3, 3)
+    const material = new THREE.MeshPhysicalMaterial({ color: '#3a7e57', metalness: 0.2, clearcoat: 1, clearcoatRoughness: 0.2  })
+
+    const positionAttribute = geometry.attributes.position
+    const index = 42
+    const y = positionAttribute.getY(index)
+    const x = positionAttribute.getX(index)
+    positionAttribute.setY(index, y + 0.15)
+    positionAttribute.setX(index, x - 0.1)
+    positionAttribute.needsUpdate = true
+
     const cube = new THREE.Mesh(geometry, material)
     cube.castShadow = true
     cube.name = 'cube'
     scene.add(cube)
+
     return cube
 }
 
-initScene(props)(({ scene, camera, renderer }) => {
+const mountSpotLight = (scene: THREE.Scene) => {
+    const light = new THREE.SpotLight(0xffffff, 3)
+
+    light.position.set(2, 2, 3)
+    light.decay = 0.5
+    light.angle = 0.3
+    light.penumbra = 0.5
+    light.shadow.mapSize.width = 2048
+    light.shadow.mapSize.height = 2048
+    light.name = 'spot-light'
+    scene.add(light)
+
+    return light
+  }
+
+initScene(props)(({ scene, camera, renderer, orbitControls }) => {
     camera.position.z = 3
- 
+
     foreverPlane(scene)
 
     mountCube(scene)
 
-    initPointerControls(camera, renderer.domElement)
+    mountSpotLight(scene)
 
     function animate() {
         requestAnimationFrame(animate)
         renderer.render(scene, camera)
+        orbitControls?.update()
         stats.update()
     }
     animate()
@@ -104,4 +66,3 @@ initScene(props)(({ scene, camera, renderer }) => {
     initSceneControls(gui, scene)
     initHelpersControls(gui, scene)
 })
-
