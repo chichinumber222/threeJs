eec29d27b666cafa647dcda573b49be0f589d08b
chichinumber222 <chichinumber222@gmail.com>
2024-11-13 21:48:24 +0300

    fix

--- src/shaders/test3/fragment.glsl
+++ src/shaders/test3/fragment.glsl
@@ -3,8 +3,10 @@ precision mediump float;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
-    vec4 texture = texture2D(uTexture, vUv);
-    gl_FragColor = texture;
+    vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation + 1.0;
+    gl_FragColor = textureColor;
 }
--- src/shaders/test3/vertex.glsl
+++ src/shaders/test3/vertex.glsl
@@ -8,14 +8,17 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.3;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.3;
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.3;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.3;
+    modelPosition.z += elevation;
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
     vUv = uv;
+    vElevation = elevation;
 }
