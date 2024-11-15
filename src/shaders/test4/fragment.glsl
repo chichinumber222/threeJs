00598630c67cb4a6962ac58ea118ad6737106f28
chichinumber222 <chichinumber222@gmail.com>
2024-11-15 04:17:27 +0300

    continue study shader pattern

--- src/shaders/test4/fragment.glsl
+++ src/shaders/test4/fragment.glsl
@@ -1,5 +1,9 @@
 varying vec2 vUv;
 
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,vec2(12.9898,78.233)))*43758.5453123);
+}
+
 void main() {
     // Pattern 3
     // float strength = vUv.x
@@ -76,7 +80,84 @@ void main() {
     // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
     // Pattern 18
-    float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+    // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    // Pattern 19
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    // Pattern 20
+    // float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float square2 = step(max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)), 0.3);
+    // float strength = square1 * square2;
+
+    // Pattern 21
+    //* floor(x) — округляет число вниз до ближайшего целого числа.
+    //* ceil(x) — округляет число вверх до ближайшего целого числа.
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+
+    // Pattern 22
+    // float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
+
+    // Pattern 23
+    // float strength = random(vUv);
+
+    // Pattern 24
+    // vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor(vUv.y * 10.0) / 10.0);
+    // float strength = random(gridUv);
+
+    // Pattern 25
+    // vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0);
+    // float strength = random(gridUv);
+
+    // Pattern 26
+    // float strength = 1.0 - (1.0 - vUv.y) * (1.0 - vUv.x);
+
+    // Pattern 26 (version 2)
+    //* length(v) - длина вектора v
+    // float strength = length(vUv);
+
+    // Pattern 27
+    // float strength = length(vUv - 0.5);
+
+    // Pattern 27 (version 2)
+    //* distance(v1, v2) - вычисляет расстояние между двумя точками или векторами v1 и v2
+    // float strength = distance(vec2(0.5), vUv);
+
+    // Pattern 28
+    // float strength = 1.0 - distance(vec2(0.5), vUv);
+
+    // Pattern 29 
+    // float strength = 0.02 / distance(vec2(0.5), vUv);
+
+    // Pattern 30
+    // vec2 lightUv = vec2(
+    //     vUv.x * 0.2 + 0.4, 
+    //     vUv.y * 0.5 + 0.25
+    // );
+    // float strength = 0.02 / distance(vec2(0.5), lightUv);
+
+    // Pattern 31 (star)
+    // vec2 lightUv1 = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+    // vec2 lightUv2 = vec2(vUv.x * 0.5 + 0.25, vUv.y * 0.1 + 0.45);
+    // float light1 = 0.02 / distance(vec2(0.5), lightUv1);
+    // float light2 = 0.02 / distance(vec2(0.5), lightUv2);
+    // float strength = light1 * light2;
+    //* work with alpha-channel; correct work with material.transparent = true;
+    // float visibility = step(0.4, strength);
+    // gl_FragColor = vec4(vec3(strength), visibility);
+
+    // Pattern 31 (star version 2)
+    // vec2 lightUv1 = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+    // vec2 lightUv2 = vec2(vUv.x * 0.5 + 0.25, vUv.y * 0.1 + 0.45);
+    // float light1 = 0.02 / distance(vec2(0.5), lightUv1);
+    // float light2 = 0.02 / distance(vec2(0.5), lightUv2);
+    // float strength = light1 * light2;
+    //* discard - no render pixel
+    // if (strength < 0.4) discard;
+    // gl_FragColor = vec4(vec3(strength), 1.0);
+
+    // Pattern 32
+    float strength = vUv.x;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
