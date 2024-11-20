f440a9f88cb6466ec1fb35e801d28ffd5457474f
chichinumber222 <chichinumber222@gmail.com>
2024-11-20 06:12:35 +0300

    finish waves - cool result

--- src/indexWaterWithShaders.ts
+++ src/indexWaterWithShaders.ts
@@ -12,8 +12,8 @@ const props: InitSceneProps = {
 
 const gui = new GUI()
 const waterColors = {
-  surface: '#76d3ea',
-  depth: '#43c3df',
+  surface: '#91dbee',
+  depth: '#18accd',
 }
 
 const createPlane = (scene: THREE.Scene) => {
@@ -23,10 +23,13 @@ const createPlane = (scene: THREE.Scene) => {
     fragmentShader: waterFragmentShader,
     uniforms: {
       uBigWavesFrequency: {
-        value: new THREE.Vector2(1, 1.5),
+        value: new THREE.Vector2(1, 1.8),
       },
-      uBigWavesDepth: {
-        value: 0.5,
+      uBigWavesElevation: {
+        value: 0.2,
+      },
+      uBigWavesSpeed: {
+        value: 3.7,
       },
       uTime: {
         value: 0,
@@ -38,10 +41,22 @@ const createPlane = (scene: THREE.Scene) => {
         value: new THREE.Color(waterColors.depth),
       },
       uColorOffset: {
-        value: 1.0,
+        value: 1.4,
       },
       uColorMultiplier: {
-        value: 0.6,
+        value: 0.5,
+      },
+      uSmallWavesSpeed: {
+        value: 0.9,
+      },
+      uSmallWavesElevation: {
+        value: 0.78,
+      },
+      uSmallWavesFrequency: {
+        value: 0.35,
+      },
+      uSmallWavesIterations: {
+        value: 4.0,
       }
     }
   })
@@ -56,16 +71,20 @@ const initHelpersWater = (gui: GUI, mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.
   const folder = gui.addFolder('Water')
   folder.add(mesh.material.uniforms.uBigWavesFrequency.value, 'x').min(0).max(5).step(0.001).name('uBigWavesFrequencyX')
   folder.add(mesh.material.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
-  folder.add(mesh.material.uniforms.uBigWavesDepth, 'value').min(0).max(5).step(0.001).name('uBigWavesDepth')
-  folder.addColor(waterColors, 'surface').onChange(() => mesh.material.uniforms.uSurfaceColor.value = new THREE.Color(waterColors.surface))
-  folder.addColor(waterColors, 'depth').onChange(() => mesh.material.uniforms.uDepthColor.value = new THREE.Color(waterColors.depth))
+  folder.add(mesh.material.uniforms.uBigWavesElevation, 'value').min(0).max(5).step(0.001).name('uBigWavesElevation')
+  folder.add(mesh.material.uniforms.uBigWavesSpeed, 'value').min(0).max(5).step(0.001).name('uBigWavesSpeed')
+  folder.add(mesh.material.uniforms.uSmallWavesFrequency, 'value').min(0).max(3).step(0.001).name('uSmallWavesFrequency')
+  folder.add(mesh.material.uniforms.uSmallWavesElevation, 'value').min(0).max(8).step(0.001).name('uSmallWavesElevation')
+  folder.add(mesh.material.uniforms.uSmallWavesSpeed, 'value').min(0).max(3).step(0.001).name('uSmallWavesSpeed')
+  folder.add(mesh.material.uniforms.uSmallWavesIterations, 'value').min(0).max(8).step(1.0).name('uSmallWavesIterations')
+  folder.addColor(waterColors, 'surface').onChange(() => mesh.material.uniforms.uSurfaceColor.value = new THREE.Color(waterColors.surface)).name('uSurfaceColor')
+  folder.addColor(waterColors, 'depth').onChange(() => mesh.material.uniforms.uDepthColor.value = new THREE.Color(waterColors.depth)).name('uDepthColor')
   folder.add(mesh.material.uniforms.uColorOffset, 'value').min(0).max(4).step(0.001).name('uColorOffset')
   folder.add(mesh.material.uniforms.uColorMultiplier, 'value').min(0).max(2).step(0.001).name('uColorMultiplier')
-  folder.close()
 }
 
 initScene(props)(({ scene, camera, renderer, orbitControls }) => {
-  camera.position.set(1, 2, 6)
+  camera.position.set(2.5, 5, 10)
 
   const water = createPlane(scene)
 
