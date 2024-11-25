5f55c19302088afdf374624a43db10544926ae2e
chichinumber222 <chichinumber222@gmail.com>
2024-11-25 15:01:48 +0300

    add texture and picture

--- src/indexSimpleShowroom.ts
+++ src/indexSimpleShowroom.ts
@@ -1,12 +1,18 @@
 import * as THREE from "three"
-import { initScene } from "./bootstrap/bootstrap"
+import { initScene, Props as InitSceneProps } from './bootstrap/bootstrap'
 import { stats } from "./utils/stats"
 import { onChangeCursor } from "./utils/update-coord"
 import gsap from 'gsap'
 import { OrbitControls } from "./controller/orbit"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import _ from 'lodash'
 
+const props: InitSceneProps = {
+  disableDefaultLights: true,
+}
+
 const textureLoader = new THREE.TextureLoader()
+const gltfLoader = new GLTFLoader()
 const raycaster = new THREE.Raycaster()
 const mouse = onChangeCursor()
 const measure = 10
@@ -43,17 +49,14 @@ const createWoodFloor = (scene: THREE.Scene) => {
   const mesh = new THREE.Mesh(
     new THREE.PlaneGeometry(10, 10),
     new THREE.MeshStandardMaterial({
-      color: 0xdddddd,
-      map: getRepeatableTexture(textureLoader.load('static/textures/wood_floor/color.jpg')),
-      normalMap: getRepeatableTexture(textureLoader.load('static/textures/wood_floor/normal.jpg')),
-      aoMap: getRepeatableTexture(textureLoader.load('static/textures/wood_floor/ao.jpg')),
-      aoMapIntensity: 3,
-      roughnessMap: getRepeatableTexture(textureLoader.load('static/textures/wood_floor/rough.jpg')),
+      color: 0xcccccc,
+      map: getRepeatableTexture(textureLoader.load('static/textures/wood-floor/color.jpg'), 10),
+      normalMap: getRepeatableTexture(textureLoader.load('static/textures/wood-floor/normal.jpg'), 10),
+      roughnessMap: getRepeatableTexture(textureLoader.load('static/textures/wood-floor/rough.jpg'), 10),
     })
   )
   mesh.rotation.x = - Math.PI * 0.5
   mesh.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(mesh.geometry.attributes.uv.array, 2))
-  mesh.receiveShadow = true
   mesh.name = 'wood-floor'
   scene.add(mesh)
   return mesh
@@ -72,7 +75,7 @@ const createCarpet = (scene: THREE.Scene) => {
     })
   )
   mesh.rotation.x = -Math.PI * 0.5
-  mesh.position.y += 0.01
+  mesh.position.y += 0.02
   mesh.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(mesh.geometry.attributes.uv.array, 2))
   mesh.receiveShadow = true
   mesh.name = 'carpet'
@@ -81,46 +84,61 @@ const createCarpet = (scene: THREE.Scene) => {
 }
 
 const createWalls = (scene: THREE.Scene) => {
-  const geometry = new THREE.PlaneGeometry(measure, measure / 2)
-  const material = new THREE.MeshStandardMaterial({ color: '#CDCA74', roughness: 0.7 })
+  const geometry = new THREE.PlaneGeometry(measure, measure / 3, 100, 100)
+  const material = new THREE.MeshStandardMaterial({
+    color: '#FCFBF4',
+    map: getRepeatableTexture(textureLoader.load('static/textures/wall/color.jpg'), 3),
+    normalMap: getRepeatableTexture(textureLoader.load('static/textures/wall/normal.jpg'), 3),
+    roughnessMap: getRepeatableTexture(textureLoader.load('static/textures/wall/rough.jpg'), 3),
+    displacementMap: getRepeatableTexture(textureLoader.load('static/textures/wall/disp.jpg'), 3),
+    displacementScale: 0.04,
+  })
   const wallNorth = new THREE.Mesh(geometry, material)
-  wallNorth.position.set(0, (measure / 4) - offset, -((measure / 2) - offset))
+  wallNorth.position.set(0, (measure / 6) - offset, -((measure / 2) - offset))
   wallNorth.name = 'wall-north'
   const wallSouth = new THREE.Mesh(geometry, material)
-  wallSouth.position.set(0, (measure / 4) - offset, (measure / 2) - offset)
+  wallSouth.position.set(0, (measure / 6) - offset, (measure / 2) - offset)
   wallSouth.rotation.set(0, Math.PI, 0)
   wallSouth.name = 'wall-south'
   const wallWest = new THREE.Mesh(geometry, material)
-  wallWest.position.set((measure / 2) - offset, (measure / 4) - offset, 0)
+  wallWest.position.set((measure / 2) - offset, (measure / 6) - offset, 0)
   wallWest.rotation.set(0, -Math.PI / 2, 0)
   wallWest.name = 'wall-west'
   const wallEast = new THREE.Mesh(geometry, material)
-  wallEast.position.set(-((measure / 2) - offset), (measure / 4) - offset, 0)
+  wallEast.position.set(-((measure / 2) - offset), (measure / 6) - offset, 0)
   wallEast.rotation.set(0, Math.PI / 2, 0)
   wallEast.name = 'wall-east'
   scene.add(wallNorth, wallSouth, wallWest, wallEast)
   return [wallNorth, wallSouth, wallWest, wallEast]
 }
 
-const createRoof = (scene: THREE.Scene) => {
+const createCeiling = (scene: THREE.Scene) => {
   const geometry = new THREE.PlaneGeometry(measure, measure)
-  const material = new THREE.MeshStandardMaterial({ color: '#F8DE7E', roughness: 0.7 })
+  const material = new THREE.MeshStandardMaterial({
+    color: '#FCFBF4',
+    map: getRepeatableTexture(textureLoader.load('static/textures/ceiling/color.jpg'), 4),
+    normalMap: getRepeatableTexture(textureLoader.load('static/textures/ceiling/normal.jpg'), 4),
+    roughnessMap: getRepeatableTexture(textureLoader.load('static/textures/ceiling/rough.jpg'), 4),
+  })
   const mesh = new THREE.Mesh(geometry, material)
   mesh.rotation.set(Math.PI / 2, 0, 0)
-  mesh.position.set(0, (measure / 2) - offset, 0)
-  mesh.name = 'roof'
+  mesh.position.set(0, (measure / 3) - offset, 0)
+  mesh.name = 'ceiling'
   scene.add(mesh)
   return mesh
 }
 
-const createTestCube = (scene: THREE.Scene) => {
-  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
-  const material = new THREE.MeshBasicMaterial({ color: '#34b5ff' })
-  const cube = new THREE.Mesh(geometry, material)
-  cube.position.set(4, 0.5, 4)
-  cube.name = 'cube'
-  scene.add(cube)
-  return cube
+const createPictureModel = (scene: THREE.Scene) => {
+  gltfLoader.load('./static/gltf/picture.gltf/fancy_picture_frame_01_2k.gltf', (gltf) => {
+    const picture = gltf.scene
+    console.log('picture :>> ', picture)
+    picture.scale.set(2.5, 2.5, 2.5)
+    picture.position.set(-2, 2, -4.85)
+    picture.castShadow = true
+    scene.add(picture)
+  }, undefined, function (error) {
+    console.error('error picture', error)
+  })
 }
 
 const createNavigationMarker = (scene: THREE.Scene) => {
@@ -180,7 +198,7 @@ const initMoveCamera = (
   const onMarkerVerification = () => {
     shot(
       (point: THREE.Vector3) => {
-        marker.position.set(point.x, point.y + 0.02, point.z)
+        marker.position.set(point.x, point.y + 0.025, point.z)
         marker.visible = true
       },
       () => marker.visible = false
@@ -248,7 +266,31 @@ const initMoveCamera = (
   })
 }
 
-initScene({})(({ scene, camera, renderer, orbitControls }) => {
+const createLight = (scene: THREE.Scene) => {
+  const ambientLight = new THREE.AmbientLight(0xffffff, 2)
+  scene.add(ambientLight)
+
+  const pointLight = new THREE.PointLight(0xffffff, 2.5)
+  pointLight.castShadow = true
+  pointLight.position.set(0, 2.5, 0)
+  scene.add(pointLight)
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2.3)
+  directionalLight.position.set(1, 2.5, 1)
+  directionalLight.shadow.camera.near = 0.1
+  directionalLight.shadow.camera.far = 20
+  directionalLight.shadow.camera.right = 5
+  directionalLight.shadow.camera.left = -5
+  directionalLight.shadow.camera.top = 5
+  directionalLight.shadow.camera.bottom = -5
+  directionalLight.shadow.mapSize.width = 1024
+  directionalLight.shadow.mapSize.height = 1024
+  directionalLight.shadow.radius = 2
+  directionalLight.shadow.bias = -0.00005
+  scene.add(directionalLight)
+}
+
+initScene(props)(({ scene, camera, renderer, orbitControls }) => {
   camera.position.set(1, 0.8, 1)
   orbitControls!.rotateSpeed = -0.5
   orbitControls!.enableZoom = false
@@ -259,12 +301,15 @@ initScene({})(({ scene, camera, renderer, orbitControls }) => {
   const bottomMeshes: THREE.Mesh[] = [
     createCarpet(scene),
     createWoodFloor(scene),
-    createTestCube(scene)
   ]
   createWalls(scene)
-  createRoof(scene)
+  createCeiling(scene)
+
+  createPictureModel(scene)
 
   initMoveCamera(scene, camera, bottomMeshes, orbitControls)
+
+  createLight(scene)
 
   function animate() {
     requestAnimationFrame(animate)
